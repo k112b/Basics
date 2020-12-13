f2d74309d7594e57d23fc56d8a4009fae60643b9
User <k112b@hotmail.com>
2020-12-13 22:45:32 +0200

    another add

---
+++ workflow_basics.R
@@ -0,0 +1,21 @@
+1/200*30
+(59+73+2)/3
+sin(pi/2)
+
+x<- 3*4
+<- 
+  x
+
+this_is_a_really_long_name <- 3.5
+
+this_is_a_really_long_name
+this
+r_rocks <- 2 ^ 3
+r_rocks
+
+
+seq(1,10)
+x <- 'hello world'
+y <- seq(1, 10, length.out = 5)
+y
+(y <- seq(1, 10, length.out = 5))
